f53e56a1d7fdf844f360dbd7038522cac4e656e1
Kyle Austad <kyleaustad@gmail.com>
2025-03-27 10:18:32 -0500

    removed unused pinia

--- app/stores/CardStore.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-import type { Card } from "~~/types/card";
-
-export const useCardStore = defineStore("Card", () => {
-  const newCard = ref<Card>();
-
-  return { newCard };
-});
